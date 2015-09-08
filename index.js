6723b75f1234d7bab4450ed3dc2b6cf9b6789a3b
Sanchit Gera <gerasanchit@gmail.com>
2015-09-07 21:53:00 -0400

    Add verbose mode

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 var program = require('commander');
 var colors = require('colors');
+var Table = require('cli-table');
+var table = new Table({
+  head: ['Formatted Address', 'Type']
+});
+var changeCase = require('change-case');
 
 program
   .version('0.0.1')
@@ -10,11 +15,20 @@ program
 
 var locateme = require('./lib/locateme');
 
-if(program.verbose) {
-}
-console.log('Initiating request...'.green)
-locateme(function(err, location, accuracy){
-  // console.log(err, location);
-  console.log('Found you!'.green);
-  console.log('You are located within a ' + accuracy + 'm radius from ' + location.results[0].formatted_address);
+locateme(program.verbose, function(err, location, accuracy) {
+  if (err) {
+    console.error(err.toString().red);
+    console.log('Aborting'.red);
+  } else if (!location) {
+    console.log('Something went wrong. Please try again!'.red);
+  } else if (program.verbose) {
+    location.results.forEach(function(result) {
+      table.push([result.formatted_address, changeCase.titleCase(result.types[0])]);
+    });
+    console.log('Here are your address components'.green);
+    console.log(table.toString());
+  } else {
+    console.log('Found you!'.green);
+    console.log('You are located within a ' + accuracy + 'm radius from ' + location.results[0].formatted_address);
+  }
 });
